1d0245a0779180d6f92ba9b8446831561ae6a373
Dumar Ojeda <dumarojeda@gmail.com>
2017-02-09 19:04:08 -0500

    Define a method that returns a formatted address in five challenge

--- challenges/5-format-address/my_solution.rb
+++ challenges/5-format-address/my_solution.rb
@@ -1,3 +1,8 @@
 # Format an Address
 
 # Tu solucion abajo:
+def make_address(street, city, state, zip)
+  "You live at #{street}, in the beautiful city of #{city}, #{state}. Your zip is #{zip}."
+end
+
+make_address("633 Folsom St.", "San Francisco", "CA", "94107")
