fbefe7e4ff45ff69d596d75f10098b975fb52860
Dumar Ojeda <dumarojeda@gmail.com>
2017-02-09 20:36:40 -0500

    Calculate letter for prom in eighth challenge

--- challenges/8-calculate-grade/my_solution.rb
+++ challenges/8-calculate-grade/my_solution.rb
@@ -1,4 +1,19 @@
 # Calculate a Grade
 
 # Tu solucion abajo:
+def get_grade(note)
+  if note >= 90
+    "A"
+  elsif note >= 80
+    "B"
+  elsif note >= 70
+    "C"
+  elsif note >= 60
+    "D"
+  else
+    "F"
+  end
+end
 
+get_grade(89) # => returns "B"
+get_grade(70) # => returns "C"
