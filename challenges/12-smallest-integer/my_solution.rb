cc317daaf046a1ac2a759f8ed5b95d4ca1b787e1
Dumar Ojeda <dumarojeda@gmail.com>
2017-02-10 20:12:32 -0500

    Find the Smallest Integer in an Array in twelfth challenge

--- challenges/12-smallest-integer/my_solution.rb
+++ challenges/12-smallest-integer/my_solution.rb
@@ -3,4 +3,11 @@
 
 
 # Tu solucion abajo:
+def smallest_integer(numbers)
+  numbers.sort!
+  numbers.first
+end
 
+smallest_integer([1, 2, 3])       # => 1
+smallest_integer([0, -10, 10])    # => -10
+smallest_integer([-10, -20, -30]) # => -30
