5292478f7eab24509c976b80f647564916d45803
Dumar Ojeda <dumarojeda@gmail.com>
2017-02-13 19:40:15 -0500

    Solution for challenge seventeen

--- challenges/17-count-between/my_solution.rb
+++ challenges/17-count-between/my_solution.rb
@@ -1,3 +1,10 @@
 # Count Between
 
 # Tu solucion abajo:
+def count_between(array, numinf, numsup)
+  if array.empty?
+    return 0
+  else
+    array.count{|x| x >= numinf && x <= numsup }
+  end
+end
