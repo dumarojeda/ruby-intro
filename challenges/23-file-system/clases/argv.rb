16aad7ff75e10f7ff75f16e74683337d0fe85141
Dumar Ojeda <dumarojeda@gmail.com>
2017-02-22 18:44:00 -0500

    Improve 23 file system

--- challenges/23-file-system/clases/argv.rb
+++ challenges/23-file-system/clases/argv.rb
@@ -1,5 +1,14 @@
 greeting_all = ARGV
 
-user = greeting_all[greeting_all.index("--user") + 1]
-greeting = greeting_all[greeting_all.index("--greeting") + 1]
-p "Hola #{user}, #{greeting}"
+index_user = greeting_all.index("--user")
+index_greeting = greeting_all.index("--greeting")
+
+if !(index_user && index_greeting)
+  p "Algo salio mal"
+else
+  user = greeting_all[index_user + 1]
+  greeting = greeting_all[index_greeting + 1]
+
+  p "Hola #{user}, #{greeting}"
+end
+
