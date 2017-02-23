b92b26ef85319abd97f972a402845007298db5ac
Dumar Ojeda <dumarojeda@gmail.com>
2017-02-22 20:08:10 -0500

    Add solution for 21 pad an array

--- challenges/21-pad-an-array/my_solution.rb
+++ challenges/21-pad-an-array/my_solution.rb
@@ -1,4 +1,26 @@
 # Mi solucion:
+def pad(array, min, phrase = nil)
+  result = array.clone
+  length = result.length
+  if min <= length
+    result
+  else    
+    new_array = Array.new(min-length,phrase)
+    result + new_array
+  end
+end
 
+def pad!(array, min, phrase = nil)
+  length = array.length
+  if min <= length
+    array
+  else
+    (length...min).each do |e|
+      array.insert(e,phrase)
+    end
+    array
+  end
+end
 
-
+pad!([1,2,3], 5)
+pad!([1,2,3], 5, 'apple')
