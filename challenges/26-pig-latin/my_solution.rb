89f40aba86e562530186cdf09af341775d201246
Dumar Ojeda <dumarojeda@gmail.com>
2017-02-24 18:59:03 -0500

    Solution 26 pig latin

--- challenges/26-pig-latin/my_solution.rb
+++ challenges/26-pig-latin/my_solution.rb
@@ -1,8 +1,26 @@
 # Pseudocode
 
-
+# rwanda
+# apple
 
 # 1. Solucion Inicial
+def pig_latin(word)
+  word.chr 
+  vowels = ["a","e","i","o","u"]
+  consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","w","x","y","z"]
+  
+  if vowels.include?(word.chr)
+    word << "ay"
+  elsif
+    new_word = word.match(/([^aeiou]+)(\w*)/)
+
+    piglatin_word = new_word[2] + new_word[1]
+
+    piglatin_word.downcase << "ay"
+  end
+
+
+end
 
 
 
@@ -10,9 +28,11 @@
 
 
 # 2. Solucion con Refactor
+  # ["a","e","i","o","u"].include?(word.chr) ? word : word.reverse << "ay"
 
 
 
 
 
 ###### DRIVER CODE #########
+p pig_latin("Rwanda")
