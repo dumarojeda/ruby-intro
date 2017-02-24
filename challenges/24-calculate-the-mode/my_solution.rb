b95fe1e96876b909a42964b2503a097ab2f779e7
Dumar Ojeda <dumarojeda@gmail.com>
2017-02-23 19:19:05 -0500

    Solution for 24 calculate the mode

--- challenges/24-calculate-the-mode/my_solution.rb
+++ challenges/24-calculate-the-mode/my_solution.rb
@@ -3,6 +3,20 @@
 
 
 # 1. Solucion Inicial
+def mode(array)
+  #array.group_by{ |e| e }
+
+  group = Hash.new(0)
+  array.each do |i|
+    group[i]+=1
+  end
+  group
+  max = group.values.max
+  output_hash = group.select {|k,v| v == max}
+  output_hash.keys
+end
+
+
 
 
 
@@ -16,3 +30,9 @@
 
 
 ###### DRIVER CODE #########
+p mode([4, 4, 5, 5, 6, 6, 6, 7, 5])         # => [3]
+# mode([4.5, 0, 0])       # => [0]
+# mode([1.5, -1, 1, 1.5]) # => [1.5]
+# mode([1,1,2,2])         # => [1,2]
+# mode([1,2,3])           # => [1,2,3], por que todos ocurren con la misma frecuencia
+# mode(["who", "what", "where", "who"]) # => "who"
