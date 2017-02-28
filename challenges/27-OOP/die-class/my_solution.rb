3b90d3b7f1d17c5a9ff79973e40e4772bb832718
Dumar Ojeda <dumarojeda@gmail.com>
2017-02-27 20:57:38 -0500

    Solution for 27 challenge die class

--- challenges/27-OOP/die-class/my_solution.rb
+++ challenges/27-OOP/die-class/my_solution.rb
@@ -3,7 +3,23 @@
 
 
 # 1. Solucion Inicial
-
+class Die
+  attr_accessor :sides
+  def initialize(sides)
+    if sides < 1
+      raise ArgumentError.new("Numero menor de 1")
+    else
+      @sides = sides    
+    end    
+  end    
+        
+  def roll
+    1 + rand(@sides)
+  end    
+end
+die = Die.new(6)
+p die.roll
+p die.sides
 
 
 
