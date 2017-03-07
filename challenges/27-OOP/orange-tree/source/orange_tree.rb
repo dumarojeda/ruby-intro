5ce158866991235c8f910a0e935bcb73f9df8c9b
Dumar Ojeda <dumarojeda@gmail.com>
2017-03-07 18:42:11 -0500

    Add solution for Orange Tree 27 Challenge

--- challenges/27-OOP/orange-tree/source/orange_tree.rb
+++ challenges/27-OOP/orange-tree/source/orange_tree.rb
@@ -3,26 +3,60 @@ class NoOrangesError < StandardError
 end
 
 class OrangeTree
+
+  attr_reader :height, :age
+
+  def initialize
+    @age = 0
+    @height = 0
+    @oranges = []
+  end
+
   # Envejeze el arbol
   def age!
+    #Agregar un año al arbol
+    @age += 1
+
+    #El arbol va creciendo hasta llegar a 2 metros
+    if @height <= 200
+      @height += rand(10..15)
+    else
+      @height
+    end
+
+    #Si el arbol ya es grande empieza a producir naranjas
+    if @age > 3
+      rand(500..600).times do |t|
+        @oranges << Orange.new(rand(10..15))
+      end
+    end
   end
 
   # Devuelve true si hay naranjas en el arbol, false si no
   def any_oranges?
+    @oranges.length > 0
   end
 
   # Devuelve una naranja si hay una
   # Dispara un NoOrangesError de lo contrario
   def pick_an_orange!
     raise NoOrangesError, "This tree has no oranges" unless self.any_oranges?
-
     # Logica para sacar naranjas va aca
+    @oranges.pop 
+  end
+
+  def dead?
+    @age >= 15
   end
 end
 
 class Orange
+
+  attr_accessor :diameter
+
   # Crea una naranja con determinado +diameter+
   def initialize(diameter)
+    @diameter = diameter
   end
 end
 
