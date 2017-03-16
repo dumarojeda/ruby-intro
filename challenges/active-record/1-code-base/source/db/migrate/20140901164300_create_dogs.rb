d65d23cca46b252f7eac51d29ae5e6be40b94855
Dumar Ojeda <dumarojeda@gmail.com>
2017-03-15 19:10:58 -0500

    Add solutions fr first and second active record

--- challenges/active-record/1-code-base/source/db/migrate/20140901164300_create_dogs.rb
+++ challenges/active-record/1-code-base/source/db/migrate/20140901164300_create_dogs.rb
@@ -1,6 +1,7 @@
 class CreateDogs < ActiveRecord::Migration
   def change
     create_table :dogs do |t|
+      t.string   :name
       t.string   :license
       t.integer  :age
       t.integer  :weight
